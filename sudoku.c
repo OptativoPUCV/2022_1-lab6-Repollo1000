d002dcddaa2b88fa15c516f7c58d3e80de8eb796
Repollo1000 <>
2022-05-12 15:42:31 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -45,16 +45,19 @@ void print_node(Node* n){
 
 int is_valid(Node* n){
 
-  int k=4,p; 
-  for(p=0;p<9;p++)
+  int array[10];
+  
+  for(int k=0 ; k<9 ; k++)
   {
-    int i=3*(k/3) + (p/3) ;
-    int j=3*(k%3) + (p%3) ;
-    printf("%d ",n->sudo[i][j]);
-    if(p%3 == 2) printf("\n");
+    int p; 
+    for(p=0;p<9;p++){
+        int i=3*(k/3) + (p/3) ;
+        int j=3*(k%3) + (p%3) ;
+        printf("%d ",n->sudo[i][j]);
+        if(p%3 == 2) printf("\n");
+    }    
   }
-
-    return 1;
+  return 1;
 }
 
 
