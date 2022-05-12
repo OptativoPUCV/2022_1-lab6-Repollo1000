6aa1e0f451077df7966b2066b3602435ffb5bd41
Repollo1000 <>
2022-05-12 22:21:57 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -47,15 +47,15 @@ int is_valid(Node* n){
   int num;
   for(int i = 0 ; i < 9 ; i++)
   {
-    //int* arreglo = (int*)calloc(10,sizeof(int));
+    int* arreglo = (int*)calloc(10,sizeof(int));
     for(int j = 0 ; j < 9 ; j++)
     {
       num = n->sudo[i][j];
-      for(int f = 0 ; f < 9 ; f++)
-      {
-        if(num == n->sudo[i][f])  
-          return 1;
-      }      
+      if(arreglo[num]==1)
+        return 0;
+      else
+        arreglo[num] = 1;
+   
     }
   }
   for(int i = 0 ; i < 9 ; i++)
@@ -67,10 +67,20 @@ int is_valid(Node* n){
       for(int f = 0 ; f < 9 ; f++)
       {
         if(num == n->sudo[f][j])  
-          return 1;
+          return 0;
       }      
     }
   }
+  for(int k = 0 ; k < 9 ; k++)
+  {
+    for(p=0;p<9;p++)
+    {
+      int i=3*(k/3) + (p/3) ;
+      int j=3*(k%3) + (p%3) ;
+      printf("%d ",n->sudo[i][j]);
+      if(p%3 == 2) printf("\n");
+    }
+  }  
   
 
   return 1;
